09c3ae2da7038a843ae0dc55fbbd25c8fdf0bfd6
itteamlxs <itbkup24@gmail.com>
2025-12-06 22:18:17 +0100

    index correct msg

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 <body class="bg-blue-900 flex items-center justify-center min-h-screen text-white">
     <div class="bg-blue-800/80 p-10 rounded-xl shadow-lg text-center">
         <h1 class="text-5xl md:text-6xl font-bold mb-6 animate-pulse text-cyan-400">🚧 Plataforma en Progreso 🚧</h1>
-        <p class="text-lg md:text-xl mb-6 text-gray-200">Estamos trabajando en algo increíble. ¡Vuelve pronto, Si!</p>
+        <p class="text-lg md:text-xl mb-6 text-gray-200">Estamos trabajando en algo increíble. ¡Vuelve pronto!</p>
         <div class="text-sm opacity-70 text-gray-300">&copy; <?= date('Y') ?> Entropic Networks</div>
     </div>
 </body>
